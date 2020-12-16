59de4fc8d58ce8b0efe7948dbe760aea16e52e15
DABax47 <45886982+DABax47@users.noreply.github.com>
2020-12-15 22:05:42 -0500

    enable lottie animations

--- js/app.js
+++ js/app.js
@@ -9,26 +9,41 @@ $(document).ready(() => {
       $home.addClass("hide-home");
     }
   };
-  // console.log($navLi);
   //home content container animation
   //get home container
   //on click of the corresponding nav item trigger animation by adding class.
   //on page load the home container is showing
   const $home = $(".home-content");
   $home.addClass("move-home");
+
+  $(".active").prepend(
+    '<lottie-player  class="lottie  " src="https://assets5.lottiefiles.com/private_files/lf30_75f9senm.json"  background="transparent"  speed="1"   loop  autoplay></lottie-player>'
+  );
   //nav animation
   //get the navigation li
   //on click add ::before element class
   const $navLi = $(".vertical-nav li");
   $navLi.on("click", (e) => {
     toggleHome(e);
-    $(e.target).addClass("active");
-    if ($(e.target).siblings().hasClass("active")) {
-      $(e.target).siblings().removeClass("active");
+    console.log(e.target);
+    if (!$(e.target).hasClass("active")) {
+      console.log("!!!!!!");
+      $(e.target)
+        .addClass("active")
+        .prepend(
+          '<lottie-player class="lottie " src="https://assets5.lottiefiles.com/private_files/lf30_75f9senm.json" mode="bounce" background="transparent"  speed="2"  "  loop  autoplay></lottie-player>'
+        )
+        .children()
+        .addClass("float");
+      if ($(e.target).siblings().hasClass("active")) {
+        $(e.target)
+          .siblings()
+          .delay(50000)
+          .removeClass("active")
+          .children()
+          .remove();
+      }
     }
-    // $(e.target).siblings().hasClass("active").removeClass("active");
-    console.log($(e.target).siblings());
-    //find some cool navigation animations to play with
   });
   //card animations
   //get the project cards
@@ -37,22 +52,8 @@ $(document).ready(() => {
   $projCard
     .mouseover((e) => {
       $(e.target).next().addClass("project-slider-move");
-      console.log(1);
     })
     .mouseout((e) => {
       $(e.target).next().removeClass("project-slider-move");
-      console.log(2);
     });
-
-  // toggle animation on when tablet or desktop or larger
-  // off on mobile devices
-  // if (window.innerWidth > 580) {
-  //   $projCards
-  //     .mouseover((e) => {
-  //       $(e.target).children().next().addClass("project-slider-move");
-  //     })
-  //     .mouseout((e) => {
-  //       $(e.target).children().next().removeClass("project-slider-move");
-  //     });
-  // }
 });
