dc510b56e2079f3b066eac707a77e6574edf376f
DABax47 <45886982+DABax47@users.noreply.github.com>
2020-12-18 14:35:12 -0500

    bulk

--- js/snow.js
+++ js/snow.js
@@ -1,9 +1,9 @@
-const $home = $(".home-content");
+const $home = $(".contact-container");
 const santaHat =
   '<img class="santa-hat" src="./images/hat.png" alt="santa hat" />';
 const snowEle =
   '<div class="snow a "></div><div class="snow b "></div><div class="snow c   "></div><div class="snow d "></div><div class="snow  e"></div><div class="snow f  "></div><div class="snow g "></div><div class="snow h"></div><div class="snow   i"></div>';
-$home.append(santaHat);
+$home.prepend(santaHat);
 $home.append(snowEle);
 const snowflake = document.querySelectorAll(".snow");
 
